bad7db67d29b29ea39659d8116a64ee504efa169
Nicholas Easter <nicholas.easter17@gmail.com>
2022-02-25 12:01:41 -0800

    added user-routes file and empty routes

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,4 +1,6 @@
 const router = require('express').Router();
 const purchaseRoutes = require('./purchase-routes');
+const userRoutes = require('./user-routes');
 
+router.use('/users', userRoutes);
 router.use('/purchases', purchaseRoutes);
