447f2a6a4c75cc5262e8a0283dbb0cfe801167ff
Nicholas Easter <nicholas.easter17@gmail.com>
2022-02-25 11:48:56 -0800

    added purchase-routes and index.js file, setup empty routes for purchase and sell history

---
+++ controllers/api/purchase-routes.js
@@ -0,0 +1,43 @@
+const router = require('express').Router();
+// import models here
+
+// uses /api/purchases
+
+// expects: http://localhost:3001/api/purchases/
+router.get('/', (req, res) => {
+    // Get the purchase history and sell history for all users
+});
+
+// expects: http://localhost:3001/api/purchases/2
+router.get('/:id', (req, res) => {
+    let id = req.params.id;
+    // Get the purchase and sell history for the user specified
+});
+
+// expects: http://localhost:3001/api/purchases/2
+// expects JSON
+// ex. { title: "Jordan's", price: 150, description: "These shoes are good" }
+// change based on table layout
+router.post('/:id', (req, res) => {
+    let id = req.params.id;
+
+    // Create a new post for the user specified by id
+})
+
+// expects: http://localhost:3001/api/purchases/2/10
+// expects similar json to post request
+router.put('/:userId/:postId', (req, res) => {
+    let userId = req.params.userId;
+    let postId = req.params.postId;
+
+    // update post based on user and post id
+
+});
+
+router.delete('/:userId/:postId', (req, res) => {
+    let userId = req.params.userId;
+    let postId = req.params.postId;
+
+    // delete post based on user and post id
+
+})
