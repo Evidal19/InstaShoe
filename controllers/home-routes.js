15a04155d367b3962a7921d6c14707a6ec3890da
Coder J <buildingguy@sbcglobal.net>
2022-02-28 15:24:08 -0800

    add handlebars for homepage

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,8 +1,36 @@
 const router = require('express').Router();
+const { User, Post, Purchase } = require('../models');
 
 // get homepage to render
 router.get('/', (req, res) => {
-    res.render('../public/img-upload.html')
+    // res.render('../public/img-upload.html')
+    Purchase.findAll({
+        attributes: { exclude: ['password'] },
+        include: {
+            model: Post,
+            attributes: ['username']
+          },
+        include: {
+          model: User,
+          attributes: ['username']
+        }
+      })
+      .then(dbPurchaseData => {
+        
+        const purchase_data = dbPurchaseData.map(blog => 
+          blog.get({ plain: true })
+        );
+  
+        console.log(purchase_data);
+  
+        res.render('homepage', {
+          purchase_data,
+        });
+      })
+      .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+      });
 });
 
 // get login page to render
