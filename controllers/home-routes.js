64f192fd0677545745993f9485710f7b7cb140d8
Coder J <buildingguy@sbcglobal.net>
2022-03-01 14:58:15 -0800

    create session on back-end

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -4,6 +4,7 @@ const { User, Post, Purchase } = require('../models');
 
 // get homepage to render
 router.get('/home', (req, res) => {
+    console.log(req.session);
     // res.render('../public/img-upload.html')
     console.log('---- GETTING PURCHASES ----');
     Purchase.findAll({
@@ -35,7 +36,11 @@ router.get('/home', (req, res) => {
 
 // get login page to render
 router.get('/login', (req, res) => {
-    res.render('login');
+  if (req.session.loggedIn) {
+    res.redirect('/home');
+    return;
+  }
+  res.render('login');
 });
 
 // get sign-up page to render
