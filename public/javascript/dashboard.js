5fb1c994f5e3cce47f827dba95bc4a1aa166d411
Elisa Vidal <eunice1vidal@yahoo.com>
2022-03-03 15:23:12 -0800

    handlebars

--- public/javascript/dashboard.js
+++ public/javascript/dashboard.js
@@ -4,15 +4,7 @@ const menu = document.querySelector(".menu");
   const closeIcon = document.querySelector(".closeIcon");
   const menuIcon = document.querySelector(".menuIcon");
 
-  // const size = {
-  //   mobileS: '320px',
-  //   mobileM: '375px',
-  //   mobileL: '425px',
-  //   tablet: '768px',
-  //   laptop: '1024px',
-  //   laptopL: '1440px',
-  //   desktop: '2560px'
-  // }
+  
 
   function toggleMenu() {
     if (menu.classList.contains("showMenu")) {
@@ -32,22 +24,4 @@ const menu = document.querySelector(".menu");
     link.addEventListener("click", toggleMenu);
   });
 
-  // export const device = {
-  //   mobileS: `(min-width: ${size.mobileS})`,
-  //   mobileM: `(min-width: ${size.mobileM})`,
-  //   mobileL: `(min-width: ${size.mobileL})`,
-  //   tablet: `(min-width: ${size.tablet})`,
-  //   laptop: `(min-width: ${size.laptop})`,
-  //   laptopL: `(min-width: ${size.laptopL})`,
-  //   desktop: `(min-width: ${size.desktop})`,
-  //   desktopL: `(min-width: ${size.desktop})`
-  // };
-
-  // const App = () => (
-  //   <>
-  //     <Hello name="CodeSandbox" />
-  //     <Card withPictureOf="cats" />
-  //     <Card withPictureOf="coffee" />
-  //     <Card withPictureOf="oranges" />
-  //   </>
-  // );
+  
