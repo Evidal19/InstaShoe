1ef2c5c27aa2bc248db0c0d320152c24e7d172e8
Coder J <buildingguy@sbcglobal.net>
2022-03-02 16:53:44 -0800

    set up front-end logic for comments

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 const sequelize = require('../config/connection');
-const { User, Post, Purchase } = require('../models');
+const { User, Post, Purchase, Comment} = require('../models');
 
 // get homepage to render
 router.get('/home', (req, res) => {
@@ -29,6 +29,56 @@ router.get('/home', (req, res) => {
         console.log(err);
         res.status(500).json(err);
       });
+});
+
+// get single post
+router.get('/post/:id', (req, res) => {
+  Post.findOne({
+    where: {
+      id: req.params.id
+    },
+    attributes: [
+      'id',
+      'post_title',
+      'post_description',
+      'file_src',
+      // 'created_at',
+      // [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
+    ],
+    include: [
+      {
+        model: Comment,
+      attributes: ['id', 'comment_text', 'post_id', 'user_id', /*'created_at'*/],
+        include: {
+          model: User,
+          attributes: ['username']
+        }
+      },
+      {
+        model: User,
+        attributes: ['username']
+      }
+    ]
+  })
+    .then(dbPostData => {
+      if (!dbPostData) {
+        res.status(404).json({ message: 'No post found with this id' });
+        return;
+      }
+
+      const post = dbPostData.get({ plain: true });
+
+      console.log(post);
+
+      res.render('single-post', {
+        post,
+        // loggedIn: req.session.loggedIn 
+      });
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 // get login page to render
