e9d99cb4bc417d9c19dd28f44f041dd1ed172a77
Elisa Vidal <eunice1vidal@yahoo.com>
2022-03-02 21:46:43 -0800

    js

--- public/javascript/add-post.js
+++ public/javascript/add-post.js
@@ -1,27 +1,53 @@
-async function newFormHandler(event) {
-    event.preventDefault();
-  
-    const title = document.querySelector('input[name=""]').value;
-    const text = document.querySelector('textarea[name=""]').value;
-  
-    const response = await fetch(`/api/posts`, {
-      method: 'POST',
-      body: JSON.stringify({
-        title,
-        text,
-        post_url
-      }),
-      headers: {
-        'Content-Type': 'application/json'
-      }
-    });
-  
-    if (response.ok) {
-      document.location.replace('/dashboard');
-    } else {
-      alert(response.statusText);
-    }
+const menu = document.querySelector(".menu");
+const navLink = document.querySelectorAll(".nav-link");
+const hamburger = document.querySelector(".hamburger");
+const closeIcon = document.querySelector(".closeIcon");
+const menuIcon = document.querySelector(".menuIcon");
+
+function toggleMenu() {
+  if (menu.classList.contains("showMenu")) {
+    menu.classList.remove("showMenu");
+    closeIcon.style.display = "none";
+    menuIcon.style.display = "block";
+  } else {
+    menu.classList.add("showMenu");
+    closeIcon.style.display = "block";
+    menuIcon.style.display = "none";
+  }
 }
+
+hamburger.addEventListener("click", toggleMenu);
+
+navLink.forEach(function (link) {
+  link.addEventListener("click", toggleMenu);
+});
+
+
+
+// async function newFormHandler(event) {
+//     event.preventDefault();
   
-document.querySelector('').addEventListener('submit', newFormHandler);
+//     const title = document.querySelector('input[name=""]').value;
+//     const text = document.querySelector('textarea[name=""]').value;
+  
+//     const response = await fetch(`/api/posts`, {
+//       method: 'POST',
+//       body: JSON.stringify({
+//         title,
+//         text,
+//         post_url
+//       }),
+//       headers: {
+//         'Content-Type': 'application/json'
+//       }
+//     });
+  
+//     if (response.ok) {
+//       document.location.replace('/dashboard');
+//     } else {
+//       alert(response.statusText);
+//     }
+// }
+  
+// document.querySelector('').addEventListener('submit', newFormHandler);
   
--- public/javascript/login.js
+++ public/javascript/login.js
@@ -1,55 +1,58 @@
 async function signupFormHandler(event) {
-    event.preventDefault();
-  
-    const username = document.querySelector('reg-username').value.trim();
-    const email = document.querySelector('email').value.trim();
-    const password = document.querySelector('reg-password').value.trim();
-  
-    if (username && email && password) {
-      const response = await fetch('/api/users', {
-        method: 'post',
-        body: JSON.stringify({
-          username,
-          email,
-          password
-        }),
-        headers: { 'Content-Type': 'application/json' }
-      });
-  
-      // check the response status
-      if (response.ok) {
-        console.log('success');
-      } else {
-        alert(response.statusText);
-      }
+  event.preventDefault();
+
+  const username = document.querySelector("reg-username").value.trim();
+  const email = document.querySelector("email").value.trim();
+  const password = document.querySelector("reg-password").value.trim();
+
+
+  if (username && email && password) {
+    const response = await fetch("/api/users", {
+      method: "post",
+      body: JSON.stringify({
+        username,
+        email,
+        password,
+      }),
+      headers: { "Content-Type": "application/json" },
+    });
+
+    // check the response status
+    if (response.ok) {
+      console.log("success");
+    } else {
+      alert(response.statusText);
     }
+  }
 }
 
 async function loginFormHandler(event) {
-    event.preventDefault();
+  event.preventDefault();
 
-    console.log('login form handler');
-  
-    const username = document.querySelector('#username').value.trim();
-    const password = document.querySelector('#password').value.trim();
-  
-    if (username && password) {
-      const response = await fetch('/api/users/login', {
-        method: 'post',
-        body: JSON.stringify({
-          username,
-          password
-        }),
-        headers: { 'Content-Type': 'application/json' }
-      });
-  
-      if (response.ok) {
-        document.location.replace('/');
-      } else {
-        alert(response.statusText);
-      }
+  console.log("login form handler");
+
+  const username = document.querySelector("#username").value.trim();
+  const password = document.querySelector("#password").value.trim();
+
+  if (username && password) {
+    const response = await fetch("/api/users/login", {
+      method: "post",
+      body: JSON.stringify({
+        username,
+        password,
+      }),
+      headers: { "Content-Type": "application/json" },
+    });
+
+    if (response.ok) {
+      document.location.replace("/");
+    } else {
+      alert(response.statusText);
     }
+  }
 }
-  
+
 // document.querySelector('#button-sign-up').addEventListener('click', signupFormHandler);
-document.querySelector('#button-login').addEventListener('click', loginFormHandler);
+document
+  .querySelector("#button-login")
+  .addEventListener("click", loginFormHandler);
