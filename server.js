1b8d13cc0dca9f1dea4c57562e0086a50cd09748
Elisa Vidal <eunice1vidal@yahoo.com>
2022-03-03 19:27:47 -0800

    updates

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ const bodyparser = require('body-parser');
 const exphbs = require('express-handlebars');
 
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3003;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
