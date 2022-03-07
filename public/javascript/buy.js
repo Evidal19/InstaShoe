499746c3e2302bb5ac22145b978460170ae18d60
Nicholas Easter <nicholas.easter17@gmail.com>
2022-03-07 15:48:00 -0800

    pulled staging

--- public/javascript/buy.js
+++ public/javascript/buy.js
@@ -9,29 +9,12 @@ const buyBtnHandler = async (event) => {
     const user_id = document.querySelector('.user_id').textContent;
 
     console.log(purchase_amount + " postId " + post_id + "userid " + user_id );
-    const response = await fetch(`/api/purchases`, {
-        method: 'POST',
-        body: JSON.stringify({
-            purchase_amount,
-            // post_id
-            post_id,
-            // user_id
-            user_id
-        }),
-        headers: {
-          'Content-Type': 'application/json'
-        }
-    });
-
-    if (response.ok) {
-        const sold_amount = document.querySelector('.purchase_amount').textContent;
-        const post_id = document.querySelector('.post_id').textContent;
-        const user_id = document.querySelector('.post_user_id').textContent;
-
-        const newResponse = await fetch(`/api/sold`, {
+    
+    if(purchase_amount && post_id && user_id){
+        const response = await fetch(`/api/purchases`, {
             method: 'POST',
             body: JSON.stringify({
-                sold_amount,
+                purchase_amount,
                 // post_id
                 post_id,
                 // user_id
@@ -41,10 +24,31 @@ const buyBtnHandler = async (event) => {
               'Content-Type': 'application/json'
             }
         });
-        alert('purchased shoe');
-    } else {
-        alert('error');
+    
+        if (response.ok) {
+            const sold_amount = document.querySelector('.purchase_amount').textContent;
+            const post_id = document.querySelector('.post_id').textContent;
+            const user_id = document.querySelector('.post_user_id').textContent;
+    
+            const newResponse = await fetch(`/api/sold`, {
+                method: 'POST',
+                body: JSON.stringify({
+                    sold_amount,
+                    // post_id
+                    post_id,
+                    // user_id
+                    user_id
+                }),
+                headers: {
+                  'Content-Type': 'application/json'
+                }
+            });
+            alert('purchased shoe');
+        } else {
+            alert('error');
+        }
     }
+    
 
 }
 
