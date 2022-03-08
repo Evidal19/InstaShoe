2bc60c10384f45e765f1c2dfb5248ffe14f8152e
Nicholas Easter <nicholas.easter17@gmail.com>
2022-03-07 16:50:23 -0800

    fixed buy buttons, they now all work and add the purchase to the database

--- public/javascript/buy.js
+++ public/javascript/buy.js
@@ -1,7 +1,5 @@
 const buyBtn = document.querySelectorAll('.buy-btn');
-const purchase_amounts = document.querySelectorAll('.purchase_amount');
-const post_ids = document.querySelectorAll('.post_id');
-const user_ids = document.querySelectorAll('.user_id');
+const posts = document.querySelectorAll(`[data-post-id]`);
 
 let purchase_amount;
 let post_id;
@@ -10,17 +8,15 @@ let user_id;
 const buyBtnHandler = async (event) => {
     event.preventDefault();
 
-    for (let i = 0; i < buyBtn.length; i++) {
-        if(document.querySelector(`[data-post-id="${i}"]`)) {
-            purchase_amount = purchase_amounts[i].textContent;
-            post_id = post_ids[i].textContent;
-            user_id = user_ids[i].textContent;
-        }
-    }
+    const postInfo = event.target.parentElement.children;
+    console.log(postInfo);
+    // get parent of button's data-post-id 
+    // then get the child elements on div
+    // then get the values
 
-    // const purchase_amount = document.querySelector('.purchase_amount').textContent;
-    // const post_id = document.querySelector('.post_id').textContent;
-    // const user_id = document.querySelector('.user_id').textContent;
+    const purchase_amount = postInfo[0].innerText;
+    const user_id = postInfo[1].innerText;
+    const post_id = postInfo[2].innerText;
 
     console.log(purchase_amount + " postId " + post_id + "userid " + user_id );
     
@@ -38,7 +34,7 @@ const buyBtnHandler = async (event) => {
               'Content-Type': 'application/json'
             }
         });
-        if (newResponse.ok) {
+        if (response.ok) {
             alert('purchased shoe');
             location.href = "/home";
         } else {
